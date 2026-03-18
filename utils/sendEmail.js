769cd5783f4f3b307493efed5d3c54b1c1003c1a
Suraj <surajkasrung231@gmail.com>
2026-03-18 11:49:30 +0530

    Refactor sendEmail to use configurable SMTP settings
    Updated email transport configuration to use environment variables for host and port.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,10 +3,12 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.EMAIL_HOST || "smtp-relay-brevo.com",
+    port: Number(process.env.EMAIL_PORT) || 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, //App password
+      pass: process.env.EMAIL_PASS,
     },
   });
 
